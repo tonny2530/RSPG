1cbf718932beef2cc5d1784b5febd3fed6a7e2ea
tonny2530 <55435714+tonny2530@users.noreply.github.com>
2019-09-23 23:55:59 +0700

    Change IP

--- app/src/main/java/th/ac/kmitl/rspg/service/InsertPictureService.java
+++ app/src/main/java/th/ac/kmitl/rspg/service/InsertPictureService.java
@@ -15,7 +15,9 @@ import th.ac.kmitl.rspg.response.InsertResponse;
 import th.ac.kmitl.rspg.util.StringUtil;
 
 public class InsertPictureService {
-    private final static String URL = "http://161.246.58.128/rspg_service/WebService.asmx?WSDL";
+//    Update New IP @23/09/2019
+    private final static String URL = "http://161.246.34.253/rspg_service/WebService.asmx?WSDL";
+//    private final static String URL = "http://161.246.58.128/rspg_service/WebService.asmx?WSDL";
     private final static String NAMESPACE = "http://tempuri.org/";
     private final static String SOAP_ACTION = "http://tempuri.org/InsertPicture";
     private final static String PARAMETER_LOG_USER_ID = "log_user_id";
