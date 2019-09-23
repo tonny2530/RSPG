bb274efc9958f4ec231afd015f86895002c8f071
tonny2530 <55435714+tonny2530@users.noreply.github.com>
2019-09-23 23:58:39 +0700

    Change IP

--- app/src/main/java/th/ac/kmitl/rspg/service/SelectAllPlantService.java
+++ app/src/main/java/th/ac/kmitl/rspg/service/SelectAllPlantService.java
@@ -16,8 +16,9 @@ import th.ac.kmitl.rspg.request.SelectAllPlantRequest;
 import th.ac.kmitl.rspg.response.SelectAllPlantResponse;
 
 public class SelectAllPlantService {
-
-    private final static String URL = "http://161.246.58.128/rspg_service/WebService.asmx?WSDL";
+//    Update New IP @23/09/2019
+    private final static String URL = "http://161.246.34.253/rspg_service/WebService.asmx?WSDL";
+//     private final static String URL = "http://161.246.58.128/rspg_service/WebService.asmx?WSDL";
     private final static String NAMESPACE = "http://tempuri.org/";
     private final static String SOAP_ACTION = "http://tempuri.org/SelectAllPlant";
     private final static String METHOD_NAME = "SelectAllPlant";
